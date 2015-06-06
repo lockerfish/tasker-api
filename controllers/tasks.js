f88c4a39ac71993965c1ad4590f3091b5997321e
Hendrix Tavarez <htavarez@gmail.com>
2015-06-06 04:45:42 -0400

    Episode 3: adding CRUD

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,67 @@
 var Task = require("../models/tasks");
 
 exports.getTasks = function (req, res) {
-	res.send("Getting all tasks");
+
+	Task.find({userId:req.params.userId}, function (err, tasks) {
+		if (err) {
+			res.send(err);
+		}
+
+		res.json(tasks);
+	});
 };
 
 exports.getTask = function (req, res) {
-	res.send("Getting a single task");
+
+	Task.find({_id:req.params.taskId}, function (err, task) {
+		if (err) {
+			res.send(err);
+		}
+
+		res.json(task);
+	})
 };
 
 exports.addTask = function (req, res) {
-	res.send("Adding a task");
+	var task = new Task();
+
+	task.taskTitle = req.body.taskTitle;
+	task.taskDescription = req.body.taskDescription;
+	task.taskStartDate = req.body.taskStartDate;
+	task.taskEndDate = req.body.taskEndDate;
+	task.userId = req.body.userId;
+
+	task.save(function (err) {
+		if(err) {
+			res.send(err);
+		}
+
+		res.send({message:"task was saved", data:task});
+	});
 };
 
 exports.updateTask = function (req, res) {
-	res.send("Updating a task>");
+	Task.update({_id:req.params.taskId}, {
+		taskTitle: req.body.taskTitle,
+		taskDescription: req.body.taskDescription,
+		taskStartDate: req.body.taskStartDate,
+		taskEndDate: req.body.taskEndDate
+	}, function (err, num, raw) {
+		if (err) {
+			req.send(err);
+		}
+
+		res.json(num);
+	});
 };
 
 exports.deleteTask = function (req, res) {
-	res.send("deleted the task");
+
+	Task.remove({_id:req.params.taskId}, function (err) {
+		if (err) {
+			res.send(err);
+		}
+
+		res.json({message: "The task was deleted"});
+	})
 };
--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-var express = require('express');
+var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 var router = express.Router();
 var taskController = require("./controllers/tasks");
+var mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/tasker");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 app.use("/api", router);
 
-router.route("/tasks").get(taskController.getTasks);
-router.route("/task").get(taskController.getTask);
+router.route("/tasks/:userId").get(taskController.getTasks);
+router.route("/task/:taskId").get(taskController.getTask);
 router.route("/addTask").post(taskController.addTask);
-router.route("/updateTask").post(taskController.updateTask);
-router.route("/deleteTask").get(taskController.deleteTask);
+router.route("/updateTask/:taskId").post(taskController.updateTask);
+router.route("/deleteTask/:taskId").get(taskController.deleteTask);
 
 app.listen(3000);
